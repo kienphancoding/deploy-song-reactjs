5e6f01c851a6d8612cc808d86e5234f86e6c06ad
kienphancoding <kientp1610@gmail.com>
2023-05-19 22:11:28 +0700

    Logic

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-"# deploy-song-reactjs" 
+## DEPLOY SONGS
+
+- Mục đích chính đưa nhạc lên netlify và dùng đường dẫn của nó để làm những ứng dụng khác (API)
+
+## Tính năng
+
+- Hiện thị bảng danh sách các bài hát : Tên bài hát , tên nghệ sĩ hoặc nhóm nhạc , năm phát hành , quốc gia , thể loại (rap , pop , ballad ,... , tiết tấu nhanh hay chậm ,...) , nhạc phim (không cần) , điệp khúc (từ mốc tgian nào đến mốc nào), nút play , pause bài hát , play chorus , name file(dùng để hiện thị cái path cho API)
---
+++ src/Songs.jsx
@@ -0,0 +1,52 @@
+const songs = [
+  {
+    name_song: "OMG",
+    name_singer: "NewJeans",
+    year: 2023,
+    category: "Rap , POP , ...",
+    country: "KPOP",
+    name_file: "NewJeans_OMG",
+    chorus_start: 40,
+    chorus_end: 60,
+    source:
+      "https://marvelous-florentine-30ea64.netlify.app/static/media/y2mate.com%20-%20Lady%20Gaga%20Bradley%20Cooper%20%20Shallow%20from%20A%20Star%20Is%20Born%20Official%20Music%20Video.4fcb30db821e3250285b.mp3",
+  },
+  {
+    name_song: "Hype Boy",
+    name_singer: "NewJeans",
+    year: 2023,
+    category: "Rap , POP , ...",
+    country: "KPOP",
+    name_file: "NewJeans_OMG",
+    chorus_start: 40,
+    chorus_end: 60,
+    source:
+      "https://marvelous-florentine-30ea64.netlify.app/static/media/y2mate.com%20-%20Sam%20Smith%20%20Unholy%20Lyrics%20ft%20Kim%20Petras%20mommy%20dont%20know%20daddys%20getting%20hot%20TikTok%20Song.9ae784a46351bacd7a66.mp3",
+  },
+  {
+    name_song: "Ditto",
+    name_singer: "NewJeans",
+    year: 2023,
+    category: "Rap , POP , ...",
+    country: "KPOP",
+    name_file: "NewJeans_OMG",
+    chorus_start: 40,
+    chorus_end: 60,
+    source:
+      "https://marvelous-florentine-30ea64.netlify.app/static/media/y2mate.com%20-%20Sam%20Smith%20%20Unholy%20Lyrics%20ft%20Kim%20Petras%20mommy%20dont%20know%20daddys%20getting%20hot%20TikTok%20Song.9ae784a46351bacd7a66.mp3",
+  },
+  {
+    name_song: "Cookie",
+    name_singer: "NewJeans",
+    year: 2023,
+    category: "Rap , POP , ...",
+    country: "KPOP",
+    name_file: "NewJeans_OMG",
+    chorus_start: 12,
+    chorus_end: 60,
+    source:
+      "https://marvelous-florentine-30ea64.netlify.app/static/media/y2mate.com%20-%20BoyWithUke%20%20Toxic.3b6153f267d770e2ee0c.mp3",
+  },
+];
+
+export { songs };
