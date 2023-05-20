17bfdc848726a00ff9a05e10f6a97b863dbaf445
kienphancoding <kientp1610@gmail.com>
2023-05-20 18:47:25 +0700

    Try to deploy

--- src/Songs.jsx
+++ src/Songs.jsx
@@ -1,49 +1,49 @@
-import NewJeans_Attention from "./assets/NewJeans_Attention.mp3";
-import NewJeans_Cookie from "./assets/NewJeans_Cookie.mp3";
-import NewJeans_Ditto from "./assets/NewJeans_Ditto.mp3";
-import NewJeans_HypeBoy from "./assets/NewJeans_HypeBoy.mp3";
-import NewJeans_OMG from "./assets/NewJeans_OMG.mp3";
+import NewJeansAttention from "./assets/NewJeansAttention.mp3";
+import NewJeansCookie from "./assets/NewJeansCookie.mp3";
+import NewJeansDitto from "./assets/NewJeansDitto.mp3";
+import NewJeansHypeBoy from "./assets/NewJeansHypeBoy.mp3";
+import NewJeansOMG from "./assets/NewJeansOMG.mp3";
 
 const songs = [
   {
     name_song: "Attention",
     name_singer: "NewJeans",
-    name_file: "NewJeans_Attention",
+    name_file: "NewJeansAttention",
     chorus_start: 62,
     chorus_end: 81,
-    source: NewJeans_Attention,
+    source: NewJeansAttention,
   },
   {
     name_song: "Cookie",
     name_singer: "NewJeans",
-    name_file: "NewJeans_Cookie",
+    name_file: "NewJeansCookie",
     chorus_start: 49,
     chorus_end: 73,
-    source: NewJeans_Cookie,
+    source: NewJeansCookie,
   },
   {
     name_song: "Ditto",
     name_singer: "NewJeans",
-    name_file: "NewJeans_Ditto",
+    name_file: "NewJeansDitto",
     chorus_start: 83,
     chorus_end: 114,
-    source: NewJeans_Ditto,
+    source: NewJeansDitto,
   },
   {
     name_song: "Hype Boy",
     name_singer: "NewJeans",
-    name_file: "NewJeans_HypeBoy",
+    name_file: "NewJeansHypeBoy",
     chorus_start: 52,
     chorus_end: 90,
-    source: NewJeans_HypeBoy,
+    source: NewJeansHypeBoy,
   },
   {
     name_song: "OMG",
     name_singer: "NewJeans",
-    name_file: "NewJeans_OMG",
+    name_file: "NewJeansOMG",
     chorus_start: 43,
     chorus_end: 75,
-    source: NewJeans_OMG,
+    source: NewJeansOMG,
   },
 ];
 
